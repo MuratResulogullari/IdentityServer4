532c58be03ac56b21bb80da6be6bcb24dda75796
murat.resulogullari <resuloglumurad@gmail.com>
2023-09-30 21:00:15 +0300

    Add project files.

---
+++ IdentityServer4.WebAppMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityServer4.WebAppMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
